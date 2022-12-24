9916b0875daffeafce34cd35e91dd87ec6f3e1f8
wonyeji <e0564722@u.nus.edu>
2022-12-24 17:56:21 +0900

    Landing Page skeleton completed

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,40 @@
 export default function Root() {
     return (
         <div className="min-h-screen flex flex-col text-white">
-            <h1>
-                This is the landing page.
-            </h1>
+            <main className="container mx-auto px-6 pt-16 flex-1 text-center">
+                <h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">
+                    Welcome to
+                </h2>
+
+                <h1 className="text-3xl md:text-6xl lg:text-8xl font-black uppercase">
+                    IMO
+                </h1>
+
+                <div className="text-sm md:text-1xl lg:text-2xl px-4 md:py-4 md:px-10 lg:py-6 
+                lg:px-12 w-fit mx-auto mb-2">
+                    Your one stop destination to find the best eateries in the world.
+                </div>
+
+                <div className="flex flex-col md:flex-row justify-center mb-2">
+                    <a href="./Login" className="bg-white bg-opacity-30 rounded-full w-fit mx-auto rounded-full md:text-3xl lg:text-4xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 cursor-pointer hover:opacity-75 duration-150">JOIN US TODAY</a>
+                </div>
+
+                <div className="opacity-85 italic">
+                    By signing up, you agree with IMO's <a href="#" className="hover:opacity-80 duration-150">Terms of Service</a> and <a href="#" className="hover:opacity-80 duration-150">Privacy Policy</a>.
+                </div>
+
+            </main>
+
+            <footer className="container mx-auto p-6 flex flex-col md:flex-row items-center justify-between">
+                <p>
+                    This is the footer.
+                </p>
+                <div className="flex -mx-6">
+                    <a href="#" className="mx-3 hover:opacity-80 duration-150">About us</a> |
+                    <a href="#" className="mx-3 hover:opacity-80 duration-150">Privacy</a> |
+                    <a href="#" className="mx-3 hover:opacity-80 duration-150">Contact</a>
+                </div>
+            </footer>
         </div>
     );
   }
